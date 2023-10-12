cc5c72278b0aa51a0e6cbe4c4742d0cffe0c7371
r-heart <rachaelkheart@gmail.com>
2023-10-12 16:15:05 -0700

    feat: :sparkles: create and render a browser router in `app.jsx`
    configure the first route to enable client side routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<div className="text-3xl font-bold underline"> Hello world!</div>} />
+  )
+  );
+
+
+
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App;
+
+
+
+
+
