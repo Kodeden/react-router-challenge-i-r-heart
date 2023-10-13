26be57e7ebe5bcd64e7e47451ad5e819d52d378c
r-heart <rachaelkheart@gmail.com>
2023-10-12 22:54:43 -0700

    feat: :sparkles: create ContactsList component

---
+++ src/components/contacts-list.jsx
@@ -0,0 +1,28 @@
+import { Link, useAsyncValue } from "react-router-dom";
+import { groupContactsByLetterUsingLastNames } from "../utils.js";
+
+export default function ContactsList() {
+    const list = useAsyncValue();
+    const orderedList = groupContactsByLetterUsingLastNames(list);
+
+    const keys = Object.keys(orderedList);
+    const values = Object.values(orderedList);
+
+
+return (
+        <div className="flex flex-col px-20 gap-y-4">
+            {keys.map((letter, index) => (
+                <div key={letter}>
+                    <h1 className="text-[#c49531] font-bold text-4xl">{letter}</h1>
+                    <ul className="text-[#e7cf9c]">
+                        {values[index].map((contact) => (
+                            <li key={contact.id}>
+                                <Link to={`contacts/${contact.id}`}>{contact.name}</Link>
+                            </li>
+                        ))}
+                    </ul>   
+                </div>
+            ))}
+        </div>
+    )
+}
