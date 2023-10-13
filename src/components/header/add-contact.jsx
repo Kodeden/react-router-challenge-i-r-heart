6ecddf0d5ba0bd63c5842e794840e92b5eb414b5
r-heart <rachaelkheart@gmail.com>
2023-10-13 15:34:01 -0700

    style: :lipstick: UI color change for button

--- src/components/header/add-contact.jsx
+++ src/components/header/add-contact.jsx
@@ -5,7 +5,7 @@ export default function AddContact() {
   return (
     <Link
       to="/add-edit"
-      className="rounded-md bg-[#a37c29] p-2 font-semibold text-[#14213d] hover:bg-[#b5bab5] hover:font-bold  hover:text-[#14213d]"
+      className="rounded-md bg-[#b5bab5] p-2 font-semibold text-[#14213d] hover:bg-[#a37c29] hover:font-bold  hover:text-[#14213d]"
     >
       Add Contact
     </Link>
