7aef30d3538603a44da35937fde6892e8cdd0c06
r-heart <rachaelkheart@gmail.com>
2023-10-12 19:11:46 -0700

    style: :lipstick: styling the Header and Outlet
    fix `Home` to `Contacts` in NavBar

--- src/components/header/add-contact.jsx
+++ src/components/header/add-contact.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function AddContact() {
     return (
-        <Link to="/add-edit" className="rounded-md p-5 bg-green-900 text-white hover:text-pink-500">
+        <Link to="/add-edit" className="rounded-md p-2 bg-[#468486] font-semibold text-[#0c0c1d] hover:bg-[#a5ffd6] hover:text-[#1b1b42]  hover:font-bold">
             Add Contact
         </Link>
     )
--- src/components/header/nav-bar.jsx
+++ src/components/header/nav-bar.jsx
@@ -3,8 +3,8 @@ import AddContact from "./add-contact";
 
 export default function NavBar() {
     return (
-        <nav className="flex justify-evenly items-center mb-5 h-25 bg-gray-900 text-white p-5">
-            <Link to="/" className="text-3xl font-bold text-white">Home</Link>
+        <nav className="flex justify-evenly items-center  bg-[#12122b] p-7">
+            <Link to="/" className="text-5xl font-bold text-[#a37c29]">Contacts</Link>
             <AddContact />
             </nav>
     );
