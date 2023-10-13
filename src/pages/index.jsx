9d0088f73fd4191cd8e1d810460b77ca6afa030f
r-heart <rachaelkheart@gmail.com>
2023-10-12 22:55:53 -0700

    feat: :sparkles: create Index.jsx with await and suspense for loading ContactsList

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
+import ContactsList from "../components/contacts-list";
+import Loading from "../components/loading";
+
+export default function Index(){
+    const contacts = useLoaderData();
+
+    return (
+        <Suspense fallsback={<Loading />}> 
+        <Await resolve={contacts}>
+            <ContactsList />
+            </Await>
+            </Suspense>
+    );
+}
