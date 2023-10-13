540e7b4dfc8220f2ec6b79f14e3d384038b4a4db
r-heart <rachaelkheart@gmail.com>
2023-10-12 22:56:35 -0700

    feat: :sparkles: render Index component in App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,15 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from "react-router-dom";
 import Error from "./components/error";
 import Layout from "./components/layout";
+import Index from "./pages/index";
+
+import contactInformation from "./pages/loaders";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}  errorElement={<Error /> } ></Route>
+    <Route path="/" element={<Layout />}  errorElement={<Error /> } >
+      <Route index element={<Index />}  loader={contactInformation} />
+    </Route>
   )
   );
 
