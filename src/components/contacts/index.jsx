521fce8eec6660ece08847be2d0c318e6f13aaa3
r-heart <rachaelkheart@gmail.com>
2023-12-06 15:26:36 -0800

    feat: :sparkles: add search function to `Contacts/index.jsx` component

--- src/components/contacts/index.jsx
+++ src/components/contacts/index.jsx
@@ -8,6 +8,14 @@ export default function ContactsList() {
 
   return (
     <div className="flex flex-col gap-y-6 px-20">
+      <input
+        type="text"
+        className="m-2 w-80 rounded-md bg-[#d0d2cd] py-3 text-[#1c371b] placeholder-[#12122b]"
+        placeholder="🔍 Search..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      ></input>
       {keys.map((letter, index) => {
         return (
           <div key={index + 30}>
