ee7d9c60d9fe9f5d35c204e7edf296c031204ec2
r-heart <rachaelkheart@gmail.com>
2023-12-06 22:23:11 -0800

    feat: :art: add all needed routes to `app.jsx`
    finish add and edit contact functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,35 @@
-
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Error from "./components/error";
 import Layout from "./components/layout";
+import { ButtonAction } from "./pages/actions";
+import ContactForm from "./pages/add-contact";
 import Index from "./pages/index";
 import contactInformation from "./pages/loaders";
 import Profile from "./pages/profile";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}  errorElement={<Error /> } >
-      <Route index element={<Index />}  loader={contactInformation} />
-      <Route path="contacts/:id" element={<Profile />}  loader={contactInformation} /> 
-    </Route>
-  )
-  );
+    <Route path="/" element={<Layout />} errorElement={<Error />}>
+      <Route index element={<Index />} loader={contactInformation} />
+      <Route
+        path="/contacts/:id"
+        element={<Profile />}
+        loader={contactInformation}
+        action={ButtonAction}
+      />
+      <Route path="add-edit" element={<ContactForm />} action={ButtonAction} />
+      <Route path="*" element={<Error />} />
+    </Route>,
+  ),
+);
 
-
-export default function App() {
+function App() {
   return <RouterProvider router={router} />;
 }
 
-
-
-
-
-
+export default App;
