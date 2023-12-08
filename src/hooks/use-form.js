0a0670d20bb96f3355b13f5f447a3353aa90cd8c
r-heart <rachaelkheart@gmail.com>
2023-12-08 09:36:00 -0800

    feat: :construction: write `use-form` hook for better handling of `add-contact` form

---
+++ src/hooks/use-form.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useForm(initialState = {}) {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const resetForm = () => {
+    setFormData(initialState);
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    resetForm();
+  };
+
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const form = formRef.current;
+    if (!form) return;
+
+    form.elements[0].focus();
+  }, [formData]);
+
+  return { formData, handleChange, resetForm, handleCancel, formRef };
+}
