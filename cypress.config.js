168f1f8a8366fecfadbb776454a1846f06f74a52
r-heart <rachaelkheart@gmail.com>
2023-12-12 12:46:12 -0800

    chore: :wrench: update `Cypress` config to use `vite`

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+import path from "path";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on(
+        "file:preprocessor",
+        vitePreprocessor({
+          configFile: path.resolve(__dirname, "./vite.config.js"),
+          mode: "development",
+        }),
+      );
     },
   },
 });
