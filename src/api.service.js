fe4d3998c05a962e4f23872e3a374aae56a3d530
r-heart <rachaelkheart@gmail.com>
2023-12-11 19:51:55 -0800

    style: :lipstick: adjust default contact image size to match the current contacts imagess

--- src/api.service.js
+++ src/api.service.js
@@ -13,7 +13,7 @@ export default {
 
   async create(newContact) {
     if (!newContact.img || newContact.img === "") {
-      newContact.img = "https://picsum.photos/id/65/200/300";
+      newContact.img = "https://picsum.photos/id/65/150/150";
     }
 
     const response = await fetch(BASE_URL, {
