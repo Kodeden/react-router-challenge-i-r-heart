ffa8c4f99ad26a8b29fb4657ffa240ea626b2a69
r-heart <rachaelkheart@gmail.com>
2023-12-11 19:35:47 -0800

    fix: :bug: update `const contacts` in `pages/index.jsx` and `const contact` in `pages/profile.jsx` in accordance with updated returned promise of the corrected `loaders.js`
    WIP fixes `loading` animated circle now showing during loading

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,15 @@
 import { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
 import ContactsList from "../components/contacts";
+import Error from "../components/error";
 import Loading from "../components/loading";
 
 export default function Index() {
-  const contacts = useLoaderData();
+  const { contacts } = useLoaderData() || {};
 
   return (
     <Suspense fallback={<Loading />}>
-      <Await resolve={contacts}>
+      <Await resolve={contacts} errorElement={<Error />}>
         <ContactsList />
       </Await>
     </Suspense>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,7 +4,7 @@ import ContactProfile from "../components/contacts/contact-profile.jsx";
 import Loading from "../components/loading";
 
 export default function Profile() {
-  const contact = useLoaderData();
+  const { contact } = useLoaderData();
 
   return (
     <Suspense fallback={<Loading />}>
