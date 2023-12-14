e4293db5eb4d4d86d5aec744747ae667cc7e6735
r-heart <rachaelkheart@gmail.com>
2023-12-14 12:32:59 -0800

    chore: :wrench: remove code with `_dirname` issue from `cypress.config.js`

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "cypress";
 import vitePreprocessor from "cypress-vite";
-import path from "path";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on) {
-      on(
-        "file:preprocessor",
-        vitePreprocessor({
-          configFile: path.resolve(__dirname, "./vite.config.js"),
-          mode: "development",
-        }),
-      );
+      on("file:preprocessor", vitePreprocessor());
     },
   },
 });
