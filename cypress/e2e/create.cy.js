abdcb7314d67308b3b87f2e8d9948d920524af73
r-heart <rachaelkheart@gmail.com>
2023-12-14 13:01:35 -0800

    test: :white_check_mark: WIP: write first E2E create test in Cypress
    test currently modifies the dbjson, need to use Cypress Fixtures next

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -2,4 +2,12 @@ it("adds a contact", () => {
   cy.visit("http://localhost:5173");
 
   cy.findByRole("link", { name: "Add Contact" }).click();
+
+  cy.findByLabelText(/Name/i).type("Joey Abrams");
+  cy.findByLabelText(/Phone/i).type("555-555-5555");
+  cy.findByLabelText(/Email/i).type("jabrams@test.com");
+
+  cy.findByRole("button", { name: "Save Contact" }).click();
+
+  cy.findByText("Joey Abrams").should("exist");
 });
